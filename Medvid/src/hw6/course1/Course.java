a51eb3f9027064c95e207174ef8c7a2914324acb
A-Medvid <medvid.koba@gmail.com>
2022-12-21 23:46:46 +0200

    hw6 - update

---
+++ Medvid/src/hw6/course1/Course.java
@@ -0,0 +1,9 @@
+package hw6.course1;
+
+public class Course {
+    public int id;
+
+    public Course(int id) {
+        this.id = id;
+    }
+}
--- Medvid/src/hw6/course1/lessons/Lection.java
+++ Medvid/src/hw6/course1/lessons/Lection.java
@@ -2,8 +2,11 @@ package hw6.course1.lessons;
 
 public class Lection {
     public int id;
+    public int courseId;
 
-    public Lection(int id) {
+    public Lection(int id, int courseId) {
         this.id = id;
+        this.courseId = courseId;
     }
+
 }
